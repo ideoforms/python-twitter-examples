e58239d33afb0d676ab652315bcc3f7aa9d7dfd9
Daniel Jones <daniel@jones.org.uk>
2018-06-28 15:45:31 +0200

    Improve authorization flow

--- twitter-authorize.py
+++ twitter-authorize.py
@@ -9,30 +9,30 @@
 import twitter
 import time
 
-print "1. Create a new Twitter application here: https://apps.twitter.com"
-print "When you have created the application, enter:"
-print "   your application name: ",
-app_name = raw_input()
+print("1. Create a new Twitter application at: https://apps.twitter.com\n")
 
-print "   your consumer key: ",
-consumer_key = raw_input()
+print("2. When you have created the application, enter:")
+print("   your application name:", end=' ')
+app_name = input()
 
-print "   your consumer secret: ",
-consumer_secret = raw_input()
+print("   your consumer key:", end=' ')
+consumer_key = input()
 
-print "2. Now, authorize this application."
-print "You'll be forwarded to a web browser in two seconds."
-print
+print("   your consumer secret:", end=' ')
+consumer_secret = input()
+
+print()
+print("3. Now, we need to authorize this application.")
+print("   You'll be forwarded to a web browser in two seconds.\n")
 
 time.sleep(2)
 
 access_key, access_secret = twitter.oauth_dance(app_name, consumer_key, consumer_secret)
 
-print "Done."
-print
-print "Now, replace the credentials in config.py with the below:"
-print
-print "consumer_key = '%s'" % consumer_key
-print "consumer_secret = '%s'" % consumer_secret
-print "access_key = '%s'" % access_key
-print "access_secret = '%s'" % access_secret
+print()
+print("4. Now, replace the credentials in config.py with the below:\n")
+
+print("consumer_key = '%s'" % consumer_key)
+print("consumer_secret = '%s'" % consumer_secret)
+print("access_key = '%s'" % access_key)
+print("access_secret = '%s'" % access_secret)
