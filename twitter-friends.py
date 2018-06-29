4c25d516c6591a886153548b541ab2c9baece156
Daniel Jones <daniel@jones.org.uk>
2018-06-29 14:45:31 +0200

    Fix users.lookup list

--- twitter-friends.py
+++ twitter-friends.py
@@ -47,14 +47,18 @@ for n in range(0, len(query["ids"]), 100):
     ids = query["ids"][n:n+100]
 
     #-----------------------------------------------------------------------
+    # create a comma-separated string from the ID list
+    #-----------------------------------------------------------------------
+    ids_string = ",".join(str(id) for id in ids)
+
+    #-----------------------------------------------------------------------
     # create a subquery, looking up information about these users
     # twitter API docs: https://dev.twitter.com/rest/reference/get/users/lookup
     #-----------------------------------------------------------------------
-    subquery = twitter.users.lookup(user_id = ids)
+    subquery = twitter.users.lookup(user_id = ids_string)
 
     for user in subquery:
         #-----------------------------------------------------------------------
         # now print out user info, starring any users that are Verified.
         #-----------------------------------------------------------------------
         print(" [%s] %s - %s" % ("*" if user["verified"] else " ", user["screen_name"], user["location"]))
-
